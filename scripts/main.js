db3fa38c157e9cd7b4e74c23862ffe77d2d06aa4
Alex Bertrand <37993142+alexanderal002@users.noreply.github.com>
2018-04-25 17:45:01 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
-//this is the blank script for actions supplied on the main page
-//code will be added as functionality becomes more relevant to the user's experience
-//thank you for your time (also, I have no idea why you're reading this, tbh)
+//This is the blank script for actions supplied on the main page.
+//Code will be added as functionality becomes more relevant to the user's
+//experience, and this script is already referenced in the HTML script.
+//Thank you for your time (also, I have no idea why you're reading this, tbh)
